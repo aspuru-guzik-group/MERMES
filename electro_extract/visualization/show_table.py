e25cfece7681981020e6a363813dfad16a6d192c
Zijian Zhang <doomspec@outlook.com>
2024-03-17 21:15:41 -0400

    improve gui

--- electro_extract/visualization/show_table.py
+++ electro_extract/visualization/show_table.py
@@ -2,31 +2,64 @@ import os
 import webbrowser
 from json2html import json2html
 
+
 def show_json_table(json_data):
     """
     Show a json data as a table in browser
     :param json_data: The json data to show
     :return:
     """
-    html = json2html.convert(json=json_data)
-    # write the html to a file
+    json_list = []
+    for key, value in json_data.items():
+        # copy the item to avoid changing the original data
+        new_value = {"name": key}
+        new_value.update(value)
+        json_list.append(new_value)
 
+    table_html = json2html.convert(json=json_list)
+    # write the html to a file
+    html = f"""
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Table</title>
+    <style>
+        table {{
+            border-collapse: collapse;
+            width: 100%;
+        }}
+
+        th, td {{
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }}
+
+        tr:nth-child(even) {{
+            background-color: #f2f2f2;
+        }}
+    </style>
+</head>
+<body>
+    {table_html}
+</body> 
+</html>
+"""
     with open("table.html", "w") as file:
         file.write(html)
     filename = 'file:///' + os.getcwd() + '/' + 'table.html'
     webbrowser.open_new_tab(filename)
 
+
 if __name__ == '__main__':
-    data = [
-        {
-            "name": "John",
+    data = {
+        "John": {
             "age": 30,
             "city": "New York"
         },
-        {
-            "name": "Alice",
+        "Alice": {
             "age": 25,
             "city": "Los Angeles"
         }
-    ]
+    }
     show_json_table(data)
